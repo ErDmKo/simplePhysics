9079d7b0a005c813f06bf51412ff2cc8284b0849
erdmko <erdmko@gmail.com>
2015-02-27 15:38:12 +0300

    Init pysics

---
+++ script.js
@@ -0,0 +1,204 @@
+var w = {},
+    main = function() {
+        var ball = new w.Ball({x:100, y:100}),
+            block = new w.Block({
+                x: 0,
+                y: a.height-10,
+                gravity: false,
+                size: new w.Vector(a.width, 10)
+            }),
+            world = new w.World(),
+            frameCounter = new w.FrameCounter(),
+            update = function() {
+                frameCounter.begin();
+                c.clearRect(0, 0, a.width, a.height)
+                world.setFps(frameCounter.getFps());
+                world.draw(c);
+                frameCounter.end();
+                requestAnimationFrame(update);
+            };
+        world.add(ball);
+        world.add(block);
+        requestAnimationFrame(update);
+    };
+(function(){
+    var startTime = Date.now(),
+        prevTime = startTime,
+        fps = 0,
+        frames = 0,
+        FrameCounter = function(){
+            
+        }
+    FrameCounter.prototype.begin = function() {
+        startTime = Date.now();
+    };
+    FrameCounter.prototype.getFps = function() {
+        return fps;
+    }
+    FrameCounter.prototype.end = function() {
+        var time = Date.now();
+        frames++;
+        if (time > prevTime) {
+            fps = Math.round((frames * 1000) / (time - prevTime))
+            prevTime = time;
+            frames = 0;
+        }
+        return time;
+    }
+    w.FrameCounter = FrameCounter;
+})();
+(function(){
+    var Vector = function(x, y){
+            this.x = x || 0;
+            this.y = y || 0;
+        },
+        typeHelper = function(args, callbacks) {
+            if (args.length == 1){
+                var n = args[0];
+                if (n instanceof Vector){
+                    return callbacks.vector ?
+                            callbacks.vector.call(this, n) :
+                            this[args.callee.name].call(this, n.x, n.y);
+                } else {
+                    return callbacks.oneArg ?
+                        callbacks.oneArg.call(this, n) :
+                        this[args.callee.name].call(this, n, n);
+                }
+            } else {
+                return callbacks.args.apply(this, args)
+            }
+        }
+    
+    Vector.prototype.copy = function copy(){
+        return new Vector(this.x, this.y);
+    }
+    Vector.prototype.scale = function scale(){
+        return typeHelper.apply(this, [arguments, {
+            args: function(x, y) {
+                return new Vector(this.x * x, this.y * y);
+            }
+        }]);
+    };
+    Vector.prototype.sub = function sub() {
+        return typeHelper.apply(this, [arguments, {
+            args: function(x, y) {
+                return new Vector(this.x - x, this.y - y);
+            }
+        }]);
+    };
+    Vector.prototype.add = function add(){
+        return typeHelper.apply(this, [arguments, {
+            args: function(x, y) {
+                return new Vector(this.x + x, this.y + y);
+            }
+        }]);
+    };
+    w.Vector = Vector;
+})();
+(function(){
+    var Polygon = function(center) {
+        this.center = center;
+        this.vertices = [];
+    };
+    Polygon.prototype.addVertex = function(v) {
+        this.addRelativeVertex(v.sub(this.center));
+    }
+    Polygon.prototype.addRelativeVertex = function(v) {
+        this.vertices.push(v);
+    }
+    w.Polygon = Polygon;
+});
+(function(){
+    var gravity = new w.Vector(0, 1).scale(9.8),
+        dumpping = -1,
+        World = function(){
+            this.objects = []; 
+            this.fps = 0;
+        }
+    World.prototype.setGravity = function(vector){
+        gravity = vector;
+    }
+    World.prototype.add = function(obj){
+        this.objects.push(obj);
+    }
+    World.prototype.setFps = function(fps){
+        this.dt = fps < 1 ? 0.0001 : 1/fps;
+    }
+    World.prototype.draw = function(c) {
+        this.objects.forEach((function(obj) {
+            if (!obj.gravity) {
+                var f = new w.Vector(0, 0),
+                    dr = obj.velocity.scale(this.dt).add(obj.acceleration.scale(0.5*this.dt*this.dt));
+                obj.move(dr.scale(10));
+                f = f.add(gravity.scale(obj.mass));
+                f = f.add(obj.velocity.scale(dumpping));
+                
+                var new_acceleration = f.scale(obj.mass),
+                    dv = obj.acceleration.add(new_acceleration).scale(0.5*this.dt);
+                obj.velocity = obj.velocity.add(dv);
+            }
+            obj.draw(c)
+        }).bind(this));
+    };  
+    w.World = World;
+})();
+(function(){
+    var id = 0,
+        WorldObj = function(options){
+            this.position = new w.Vector(options.x, options.y);
+            this.gravity = typeof options.gravity === "undefined"? options.gravity : true;
+            this.collision = typeof options.gravity === "undefined"? true : false;
+            this.mass = 1;
+            this.velocity = new w.Vector(0, 0);
+            this.acceleration =new  w.Vector(0, 0);
+            this.id = id++;
+        };
+    WorldObj.prototype.drawObjShape = function(c) {
+        throw 'Empty shape';
+    }
+    WorldObj.prototype.testCollision = function(objectsList){
+        throw 'Empty collision shape'; 
+    }
+    WorldObj.prototype.move = function(v) {
+        this.position = this.position.add(v);
+    }
+    WorldObj.prototype.draw = function(c){
+        c.save();
+        c.beginPath();
+        this.drawObjShape(c);
+        c.fillStyle = 'green';
+        c.fill();
+        c.closePath();
+        c.restore();
+    }
+    w.WorldObj = WorldObj;
+})();
+(function(){
+    var private_vars = {},
+        Ball = function(options) {
+            w.WorldObj.apply(this, arguments); 
+            private_vars[this.id] = {
+                radius: options.radius || 20,
+            }
+        };    
+    Ball.prototype = Object.create(w.WorldObj.prototype);
+    Ball.prototype.drawObjShape = function(c){
+        c.arc(this.position.x, this.position.y, private_vars[this.id].radius, 0, 2*Math.PI, false);
+    }
+    w.Ball = Ball;
+})();
+(function(){
+    var private_vars = {},
+        Block = function(options){
+            w.WorldObj.apply(this, arguments); 
+            private_vars[this.id] = {
+                size: options.size? options.size : new w.Vector(10, 10)   
+            }
+        };
+    Block.prototype = Object.create(w.WorldObj.prototype);
+    Block.prototype.drawObjShape = function(c) {
+        c.fillRect(this.position.x, this.position.y, private_vars[this.id].size.x, private_vars[this.id].size.y);
+    }
+    w.Block = Block;
+})();
+main();
