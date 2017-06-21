b2a7be69ebdb390db3d98caf114a97e146288fad
erdmko <erdmko@gmail.com>
2017-06-21 22:39:35 +0300

    FrameCounter

---
+++ src/FrameCounter.ts
@@ -0,0 +1,25 @@
+export class FrameCounter  {
+    constructor(
+        private startTime: number = 0,
+        private prevTime: number = 0,
+        private frames: number = 0,
+        private fps: number = 0
+    ) {}
+    begin(): FrameCounter {
+        this.startTime = Date.now();
+        return this
+    }
+    getFps(): number {
+        return this.fps;
+    }
+    end(): number {
+        let time = Date.now();
+        this.frames++;
+        if (time > this.prevTime) {
+            this.fps = Math.round((this.frames * 1000) / (time - this.prevTime))
+            this.prevTime = time;
+            this.frames = 0;
+        }
+        return time;
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { Vector } from './Vector'
+import { FrameCounter } from './FrameCounter'
 
+const fc = new FrameCounter()
+fc.begin()
 console.log('hello world');
 new Vector(1,1).add(new Vector(1, 1))
+fc.end()
